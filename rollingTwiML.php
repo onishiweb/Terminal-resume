91fec739153246c140888dfd7a424b820919965d
Adam Onishi <aonishi@wearearchitect.com>
2014-12-09 13:34:33 +0000

    Add twilio require into the TwiML php

--- rollingTwiML.php
+++ rollingTwiML.php
@@ -1,4 +1,8 @@
 <?php
+// Install the library via PEAR or download the .zip file to your project folder.
+// This line loads the library
+require('twilio/Services/Twilio.php');
+
 $response = new Services_Twilio_Twiml();
 $response->play('http://adamonishi.com/resume/assets/astley.mp3');
 $response->say('You\'ve just been Rickrolled by Adam Onishi');
