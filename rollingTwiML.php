6209234f0826be213f8a26ddbdef29d69280af2d
Adam Onishi <aonishi@wearearchitect.com>
2014-12-09 13:52:27 +0000

    Update TwiML and add mp3

--- rollingTwiML.php
+++ rollingTwiML.php
@@ -5,6 +5,5 @@ require('twilio/Services/Twilio.php');
 
 $response = new Services_Twilio_Twiml();
 $response->play('http://adamonishi.com/resume/assets/astley.mp3');
-$response->say('You\'ve just been Rickrolled by Adam Onishi');
 print $response;
 ?>
